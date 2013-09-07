e97e62b207c5161f09ed5941f0a68ca228170a88
Wenjia <wengia_mawenjia@hotmail.com>
2013-09-07 12:07:49 -0700

    hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WenBlog::Application.config.secret_key_base = '8dcdfb537d7a2b48e7405172167cfa600566ae66a3e4560bf3d163721a2510c84982d4a2a4f79f314e3285cbbd48b394f23b327ae2a0542c25edbc74cbd2780f'
